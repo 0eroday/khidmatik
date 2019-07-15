08fc890050ed9ca51ce988c5a03fcef629f2b763
Mohammed kasem <mohamedtarbosh1@gmail.com>
2019-07-15 13:18:02 +0300

    creat v1 server

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-const ex = require('express');
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(morgan('dev'));
+
+app.listen(PORT,()=>{console.log("server now runing on port " +`${PORT}`);
+})
