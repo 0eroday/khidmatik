bc54f8ca13bd2749c3ee3f79b6f956afb5b7b7e8
Mohammed kasem <mohamedtarbosh1@gmail.com>
2019-07-16 14:02:02 +0300

    update/user registration

--- route/api/user.js
+++ route/api/user.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const {check , validationResult} = require('express-validator/check');
+const User = require('../../db-models/User');
+const bcrypt = require('bcryptjs');
+
+
+
 // @route    POST api/users
 // @desc     Register user
 // @access   Public
@@ -14,13 +19,35 @@ router.post('/',
     check('password','Please enter a password with 6 or more characters')
       .isLength({ min: 6 })
   ],
-  (req, res)=>{
+  async (req, res)=>{
     const errors = validationResult(req);
     if(!errors.isEmpty()){
       return res.status(400).json({errors: errors.array()});
-    }     
-    res.send('zin')
+    }  
+    
+    const {name, email, password} = req.body;
+    try{
+      let user = await User.findOne({email})
+      if(user){
+        return res.status(400).json({errors: {msg: "User is already exists"}});
+      }
+
+      user = new User({
+        name,
+        email,
+        password
+      });
+
+      const sult = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, sult);
+      await user.save();
+
+      res.send('user is on bro')
+    }catch(err){
+      console.log(err.message);
+      res.status(500).send('Server error');
+    }
   });
 
 
-module.exports = router;
+module.exports = router;  
